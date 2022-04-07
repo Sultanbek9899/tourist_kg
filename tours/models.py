e37a3e68051a7fd9ffa3c276a5f813efb963623e
Sultan Beksultanov <sultanbek9899@gmail.com>
2022-04-07 21:10:20 +0600

    add model for booking

--- tours/models.py
+++ tours/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Tour(models.Model):
     title = models.CharField("Название",max_length=100)
@@ -25,10 +25,10 @@ class RegularTour(models.Model):
     TOUR_STATUS_COMPLETED = 'completed'
     TOUR_STATUS_REJECTED = 'rejected'
     TOUR_STATUSES = (
-        ("Идет набор", TOUR_STATUS_WAITING),
-        ("Начался", TOUR_STATUS_START),
-        ("Завершен", TOUR_STATUS_COMPLETED),
-        ("Отменен", TOUR_STATUS_REJECTED),
+        (TOUR_STATUS_WAITING,"Идет набор"),
+        (TOUR_STATUS_START, "Начался"),
+        (TOUR_STATUS_COMPLETED,"Завершен",),
+        (TOUR_STATUS_REJECTED,"Отменен"),
     )
     tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
     start = models.DateTimeField("Начало")
@@ -51,4 +51,49 @@ class RegularTour(models.Model):
         return f"{self.tour.title-self.start}"
 
         
+class TourBooking(models.Model):
+    STATUS_NEW = "new"
+    STATUS_CONFIRMED = "confirmed"
+    STATUS_FINISHED = "finished"
+    STATUS_REJECTED = "rejected"
+    BOOKING_STATUSES = (
+        (STATUS_NEW, "Новый"),
+        (STATUS_CONFIRMED,"Подтвержден"),
+        (STATUS_FINISHED, "Завершен",),
+        (STATUS_REJECTED, "Отменен",)
+    )
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.DO_NOTHING
+        )
+    regular_tour = models.ForeignKey(
+        RegularTour,
+        on_delete=models.DO_NOTHING
+    )
+    place_count = models.PositiveSmallIntegerField("Места")
+    mobile = models.CharField("Номер телефона", max_length=10)
+    status = models.CharField(
+        "Статус",
+        max_length=9,
+        choices=BOOKING_STATUSES,
+        default=STATUS_NEW 
+    )
+    is_paid = models.BooleanField("Оплачено", default=False)
+    notice = models.CharField(
+        "Доп. инфо", 
+        max_length=255,
+        null=True, 
+        blank=True
+    )
 
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name = "Бронирование"
+        verbose_name_plural = "Бронирования"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f"Бронь {self.id}"
